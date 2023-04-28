2b043e6790f5c835934f23258dc494c89a442306
jamesh1983 <jamesh2@163.com>
2023-04-28 17:33:24 +0800

    20230428

--- Modbus.Server/Server.Designer.cs
+++ Modbus.Server/Server.Designer.cs
@@ -29,18 +29,25 @@
         private void InitializeComponent()
         {
             this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.userButton2 = new HslCommunication.Controls.UserButton();
             this.label_times = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.userButton1 = new HslCommunication.Controls.UserButton();
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.label1 = new System.Windows.Forms.Label();
             this.textBox2 = new System.Windows.Forms.TextBox();
-            this.userButton2 = new HslCommunication.Controls.UserButton();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
             // 
             // groupBox1
             // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.userButton2);
             this.groupBox1.Controls.Add(this.label_times);
             this.groupBox1.Controls.Add(this.label2);
@@ -54,12 +61,44 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "配置";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(597, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(17, 12);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "--";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(537, 20);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(65, 12);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "数据长度：";
+            // 
+            // userButton2
+            // 
+            this.userButton2.BackColor = System.Drawing.Color.Transparent;
+            this.userButton2.CustomerInformation = "";
+            this.userButton2.EnableColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(190)))), ((int)(((byte)(190)))));
+            this.userButton2.Font = new System.Drawing.Font("微软雅黑", 9F);
+            this.userButton2.Location = new System.Drawing.Point(233, 23);
+            this.userButton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.userButton2.Name = "userButton2";
+            this.userButton2.Size = new System.Drawing.Size(107, 25);
+            this.userButton2.TabIndex = 17;
+            this.userButton2.UIText = "停止服务";
+            this.userButton2.Click += new System.EventHandler(this.userButton2_Click);
+            // 
             // label_times
             // 
             this.label_times.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.label_times.Location = new System.Drawing.Point(486, 29);
             this.label_times.Name = "label_times";
-            this.label_times.Size = new System.Drawing.Size(101, 14);
+            this.label_times.Size = new System.Drawing.Size(45, 14);
             this.label_times.TabIndex = 16;
             this.label_times.Text = "0";
             this.label_times.TextAlign = System.Drawing.ContentAlignment.TopRight;
@@ -112,28 +151,44 @@
             this.textBox2.Multiline = true;
             this.textBox2.Name = "textBox2";
             this.textBox2.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
-            this.textBox2.Size = new System.Drawing.Size(700, 364);
+            this.textBox2.Size = new System.Drawing.Size(697, 124);
             this.textBox2.TabIndex = 1;
             // 
-            // userButton2
+            // listBox1
             // 
-            this.userButton2.BackColor = System.Drawing.Color.Transparent;
-            this.userButton2.CustomerInformation = "";
-            this.userButton2.EnableColor = System.Drawing.Color.FromArgb(((int)(((byte)(190)))), ((int)(((byte)(190)))), ((int)(((byte)(190)))));
-            this.userButton2.Font = new System.Drawing.Font("微软雅黑", 9F);
-            this.userButton2.Location = new System.Drawing.Point(233, 23);
-            this.userButton2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.userButton2.Name = "userButton2";
-            this.userButton2.Size = new System.Drawing.Size(107, 25);
-            this.userButton2.TabIndex = 17;
-            this.userButton2.UIText = "停止服务";
-            this.userButton2.Click += new System.EventHandler(this.userButton2_Click);
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(13, 216);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(120, 304);
+            this.listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.ItemHeight = 12;
+            this.listBox2.Location = new System.Drawing.Point(139, 216);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(120, 304);
+            this.listBox2.TabIndex = 3;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.ItemHeight = 12;
+            this.listBox3.Location = new System.Drawing.Point(265, 216);
+            this.listBox3.Name = "listBox3";
+            this.listBox3.Size = new System.Drawing.Size(120, 304);
+            this.listBox3.TabIndex = 4;
             // 
             // Server
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(724, 461);
+            this.ClientSize = new System.Drawing.Size(721, 530);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
             this.Controls.Add(this.textBox2);
             this.Controls.Add(this.groupBox1);
             this.Name = "Server";
@@ -157,6 +212,11 @@
         private HslCommunication.Controls.UserButton userButton1;
         private System.Windows.Forms.TextBox textBox2;
         private HslCommunication.Controls.UserButton userButton2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
     }
 }
 
--- Modbus.Server/Server.cs
+++ Modbus.Server/Server.cs
@@ -52,13 +52,37 @@ namespace Modbus.Server
             //textBox2.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.f") + " :" +
             //    HslCommunication.BasicFramework.SoftBasic.ByteToHexString(modbus) + Environment.NewLine);
             string tempstring = "";
+            float[] float_data = ByteArray_To_FloatArray(modbus,13,80);
+            bool[] bool_data = ByteArray_To_BoolArray(modbus,93,8);
+            int[] int_data = ByteArray_To_IntArray(modbus,101,112);
+            label4.Text = modbus.Length.ToString();
+            //int a = 0, b = 0, c = 0;
             for (int i = 0; i < modbus.Length; i++)
             {
-                tempstring += modbus[i].ToString();
+                //if (i > 92 && i <= 100) 
+                //{
+                //    bool_data[b] = modbus[i];
+                //tempstring += modbus[i].ToString();
+                
+                //if (i <= 92 && i > 12)
+                //{
+                //    float_data[a] = modbus[i];
+                //    a++;
+                //}
+                //    b++;
+                //}
+                //if (i > 100 && i <= 213)
+                //{
+                //    int_data[c] = modbus[i];
+                //    c++;
+                //}
             }
             textBox2.AppendText(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.f") + " :" + 
                 tempstring + 
                 Environment.NewLine);
+            listBox1.DataSource = float_data;
+            listBox2.DataSource = bool_data;
+            listBox3.DataSource = int_data;
         }
         
         private Timer timer = new Timer();
@@ -83,6 +107,84 @@ namespace Modbus.Server
             textBox1.Enabled = true;
             userButton1.Enabled = true;
             userButton2.Enabled = false;
+        }
+
+        private bool[] ByteArray_To_BoolArray(byte[] sourcearray, int sourceindex, int arraylength)
+        {
+            bool[] destinationarray = new bool[arraylength*8];
+            if (sourcearray.Length >= arraylength)
+            {
+                byte[] temp_bool = new byte[arraylength];
+                destinationarray = new bool[arraylength * 8];
+                Array.Copy(sourcearray, sourceindex, temp_bool, 0, arraylength);
+                for (int i = 0; i < temp_bool.Length; i++)
+                {
+                    int tempdata = temp_bool[i];
+                    for (int j = 0; j < 8; j++)
+                    {
+                        int x = Convert.ToInt16(Math.Pow(2, j));
+                        tempdata = temp_bool[i] & x;
+                        destinationarray[i * 8 + j] = Convert.ToBoolean(tempdata);
+                    }
+                }
+            }
+            else MessageBox.Show("数据长度错误！");
+            return destinationarray;
+        }
+
+        private float[] ByteArray_To_FloatArray(byte[] sourcearray, int sourceindex, int arraylength)
+        {
+            float[] destinationarray = new float[arraylength / 4];
+            if ((sourcearray.Length >= arraylength) && ((arraylength % 4) == 0))
+            {
+                byte[] temp_byte = new byte[arraylength];
+                destinationarray = new float[arraylength / 4];
+                Array.Copy(sourcearray, sourceindex, temp_byte, 0, arraylength);
+                for (int i = 0; i < arraylength; i = i + 4)
+                {
+                    byte[] bytedata = { temp_byte[i + 3], temp_byte[i + 2], temp_byte[i + 1], temp_byte[i] };
+                    destinationarray[i / 4] = Byte_To_Float(bytedata);
+                }
+            }
+            else MessageBox.Show("数据长度错误！");
+            return destinationarray;
+        }
+
+        private int[] ByteArray_To_IntArray(byte[] sourcearray, int sourceindex, int arraylength)
+        {
+            int[] destinationarray = new int[arraylength / 2];
+            if ((sourcearray.Length >= arraylength) && ((arraylength % 2) == 0))
+            {
+                byte[] temp_bool = new byte[arraylength];
+                destinationarray = new int[arraylength / 2];
+                Array.Copy(sourcearray, sourceindex, temp_bool, 0, arraylength);
+                for (int x = 0; x < arraylength; x = x + 2)
+                {
+                    destinationarray[x / 2] = temp_bool[x] << 8 | temp_bool[x + 1];
+                }
+            }
+            else MessageBox.Show("数据长度错误！");
+            return destinationarray;
+        }
+
+        private static float Byte_To_Float(byte[] data)
+        {
+            unsafe
+            {
+                float a = 0.0F;
+                byte i;
+                byte[] x = data;
+                void* pf;
+                fixed (byte* px = x)
+                {
+                    pf = &a;
+                    for (i = 0; i < data.Length; i++)
+                    {
+                        *((byte*)pf + i) = *(px + i);
+                    }
+                }
+                return a;
+            }
         }
     }
 }
